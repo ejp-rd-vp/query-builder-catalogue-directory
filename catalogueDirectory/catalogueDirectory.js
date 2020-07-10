20bd56598448ea16817409b79343bef33311e980
David Reinert <david.reinert@kgu.de>
2020-07-10 12:10:55 +0200

    added db.json file

--- catalogueDirectory/catalogueDirectory.js
+++ catalogueDirectory/catalogueDirectory.js
@@ -9,9 +9,29 @@ const HOST = 'localhost';
 
 // define and run the server application
 const app = express();
-app.listen(PORT, HOST, () => console.log(`Catalogue Directory listening on http://${HOST}:${PORT} ...`));
-app.get('/', (req, res) => {
-  res.send('EJP - Central query portal - Catalogue directory');
+app.listen(PORT, () => console.log(`Catalogue Directory listening on http://${HOST}:${PORT} ...`));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.json({limit: '1mb'}));
+
+app.post('/catalogues', (request, response, next) => {
+  console.log(request.body);
+  response.json({
+    status: 'success',
+    echo: request.body
+  });
+});
+
+app.get('/', (request, response, next) => {
+  response.send('EJP - Central query portal - Catalogue directory');
+});
+
+app.get('/catalogues', (request, response, next) => {
+  response.send('/catalogues/db.json');
 });
 
 
