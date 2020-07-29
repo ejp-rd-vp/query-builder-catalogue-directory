9f65b1e8872c3e2e1942753e4ad13db94afad76b
David Reinert <david.reinert@kgu.de>
2020-07-29 15:58:14 +0200

    added object orientation

--- catalogueDirectory.js
+++ catalogueDirectory.js
@@ -12,140 +12,124 @@ const cors = require("cors");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 
-// create and load the catalogue database
-const catalogueDatabase = new Datastore("./catalogues.json");
-catalogueDatabase.loadDatabase();
+// class that holds a NeDB database and its' functionality
+exports.Directory = class Directory {
+  catalogueDatabase;
 
-// define the server application
-const app = express();
+  constructor(dataBaseFilename) {
+    this.catalogueDatabase = new Datastore(dataBaseFilename);
+    this.catalogueDatabase.loadDatabase();
+  }
 
-// define https config
-const key = fs.readFileSync("private.key");
-const cert = fs.readFileSync("certificate.crt");
-const credentials = {
-  key: key,
-  cert: cert,
+  getDirectory() {
+    return this.catalogueDatabase;
+  }
 };
 
-// add helmet to enhance security
-app.use(helmet());
+// class that holds an express application and its' configuration
+exports.Application = class Application {
+  app;
+  catalogueDatabase;
+  checkJwt;
 
-// add morgan to log HTTP requests
-app.use(morgan("dev"));
-
-// add cross origin resource sharing
-app.use(cors());
-
-// create json web token validation
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://dev-luoogqm3.eu.auth0.com/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer.
-  audience: "http://express.api",
-  issuer: `https://dev-luoogqm3.eu.auth0.com/`,
-  algorithms: ["RS256"],
-});
-
-// configure express server options
-app.use(
-  express.json({
-    limit: "1mb",
-  })
-);
-
-// configure and run the server
-setup();
-
-// function that configures and runs the application
-function setup() {
-  try {
-    // parse command line arguments
-    var commandLineArguments = process.argv.slice(2);
-
-    // check for proper usage
-    if (commandLineArguments.length != 2) {
-      console.error("Usage: node catalogueDirectory.js $PORT");
-      return;
-    } else {
-      const httpServer = http.createServer(app);
-      const httpsServer = https.createServer(credentials, app);
-      // run the server application
-      httpServer.listen(commandLineArguments[0], () =>
-        console.log(
-          `Catalogue Directory listening on http://localhost:${commandLineArguments[0]} ...`
-        )
-      );
-      httpsServer.listen(commandLineArguments[1], () =>
-        console.log(
-          `Catalogue Directory listening on https://localhost:${commandLineArguments[1]} ...`
-        )
-      );
-    }
-  } catch (exception) {
-    console.error(
-      "Error in catalogueDirectory:catalogueDirectory.js:setup(): ",
-      exception
+  constructor(database) {
+    this.app = express();
+    this.app.use(helmet());
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(
+      express.json({
+        limit: "1mb",
+      })
     );
-  }
-}
 
-// add root GET route
-app.get("/", (request, response, next) => {
-  try {
-    response.sendStatus(200);
-  } catch (exception) {
-    console.error(
-      "Error in catalogueDirectory:catalogueDirectory.js:app.get(/): ",
-      exception
-    );
-  }
-});
+    this.catalogueDatabase = database;
 
-// add GET route for the query catalogues functionality
-app.get("/getCatalogues", (request, response, next) => {
-  try {
-    catalogueDatabase.find({}, (err, data) => {
-      if (err) {
-        response.end();
-        return;
-      }
-      response.json(data);
+    this.checkJwt = jwt({
+      secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-luoogqm3.eu.auth0.com/.well-known/jwks.json`,
+      }),
+
+      // Validate the audience and the issuer.
+      audience: "http://express.api",
+      issuer: `https://dev-luoogqm3.eu.auth0.com/`,
+      algorithms: ["RS256"],
     });
-  } catch (exception) {
-    console.error(
-      "Error in catalogueDirectory:catalogueDirectory.js:app.get(/getCatalogues): ",
-      exception
+
+    this.app.get("/", (request, response, next) => {
+      try {
+        response.sendStatus(200);
+        return 0;
+      } catch (exception) {
+        console.error(
+          "Error in catalogueDirectory:catalogueDirectory.js:app.get(/): ",
+          exception
+        );
+      }
+    });
+
+    exports.getCatalogues = this.app.get(
+      "/getCatalogues",
+      (request, response, next) => {
+        try {
+          this.catalogueDatabase.find({}, (err, data) => {
+            if (err) {
+              response.end();
+              return -1;
+            }
+            response.json(data);
+            return 0;
+          });
+        } catch (exception) {
+          console.error(
+            "Error in catalogueDirectory:catalogueDirectory.js:app.get(/getCatalogues): ",
+            exception
+          );
+        }
+      }
     );
-  }
-});
 
-// use json web token authentication for POST requests
-app.use(checkJwt);
-
-// add POST route for the add catalogues functionality
-app.post("/addCatalogue", (request, response, next) => {
-  try {
-    let nameExists = Boolean(false);
-    const data = request.body;
-    catalogueDatabase.find(
-      { catalogueName: { $in: [data.catalogueName] } },
-      (error, records) => {
-        if (error) {
-          console.error(error);
-        } else if (records.length > 0) {
-          nameExists = true;
-          catalogueDatabase.find(
-            { catalogueAddress: { $in: [data.catalogueAddress] } },
+    exports.addCatalogues = this.app.post(
+      "/addCatalogue",
+      (request, response, next) => {
+        try {
+          let nameExists = Boolean(false);
+          const data = request.body;
+          this.catalogueDatabase.find(
+            { catalogueName: { $in: [data.catalogueName] } },
             (error, records) => {
               if (error) {
                 console.error(error);
-              } else if (records.length > 0 && nameExists) {
-                response.sendStatus(400);
+                return -1;
+              } else if (records.length > 0) {
+                nameExists = true;
+                this.catalogueDatabase.find(
+                  { catalogueAddress: { $in: [data.catalogueAddress] } },
+                  (error, records) => {
+                    if (error) {
+                      console.error(error);
+                    } else if (records.length > 0 && nameExists) {
+                      response.sendStatus(400);
+                    } else {
+                      const timestamp = Date.now();
+                      data.timestamp = timestamp;
+                      data._id = Math.random()
+                        .toString(36)
+                        .substr(2, 9)
+                        .toUpperCase();
+                      this.catalogueDatabase.insert(data);
+                      this.catalogueDatabase.persistence.compactDatafile();
+                      response.json({
+                        status: "success",
+                        id: data._id,
+                      });
+                      return 0;
+                    }
+                  }
+                );
               } else {
                 const timestamp = Date.now();
                 data.timestamp = timestamp;
@@ -153,65 +137,134 @@ app.post("/addCatalogue", (request, response, next) => {
                   .toString(36)
                   .substr(2, 9)
                   .toUpperCase();
-                catalogueDatabase.insert(data);
-                catalogueDatabase.persistence.compactDatafile();
+                this.catalogueDatabase.insert(data);
+                this.catalogueDatabase.persistence.compactDatafile();
                 response.json({
                   status: "success",
                   id: data._id,
                 });
+                return 0;
               }
             }
           );
-        } else {
-          const timestamp = Date.now();
-          data.timestamp = timestamp;
-          data._id = Math.random().toString(36).substr(2, 9).toUpperCase();
-          catalogueDatabase.insert(data);
-          catalogueDatabase.persistence.compactDatafile();
-          response.json({
-            status: "success",
-            id: data._id,
-          });
+        } catch (exception) {
+          console.error(
+            "Error in catalogueDirectory:catalogueDirectory.js:app.post(/addCatalogue): ",
+            exception
+          );
         }
       }
     );
-  } catch (exception) {
-    console.error(
-      "Error in catalogueDirectory:catalogueDirectory.js:app.post(/addCatalogue): ",
-      exception
-    );
-  }
-});
 
-// add POST route for the add catalogues functionality
-app.post("/removeCatalogue", (request, response, next) => {
-  try {
-    const data = request.body;
-    catalogueDatabase.find({ _id: { $in: [data.catalogueID] } }, function (
-      error,
-      docs
-    ) {
-      if (error) {
-        console.error(error);
-      } else if (!docs.length) {
-        response.sendStatus(404);
-      } else {
-        catalogueDatabase.remove({ _id: data.catalogueID }, {}, (error) => {
-          if (error) {
-            console.error(error);
-          } else {
-            catalogueDatabase.persistence.compactDatafile();
-            response.json({
-              status: "success",
-            });
-          }
-        });
+    exports.removeCatalogues = this.app.post(
+      "/removeCatalogue",
+      (request, response, next) => {
+        try {
+          const data = request.body;
+          this.catalogueDatabase.find(
+            { _id: { $in: [data.catalogueID] } },
+            (error, docs) => {
+              if (error) {
+                console.error(error);
+                return -1;
+              } else if (!docs.length) {
+                response.sendStatus(404);
+                return 404;
+              } else {
+                this.catalogueDatabase.remove(
+                  { _id: data.catalogueID },
+                  {},
+                  (error) => {
+                    if (error) {
+                      console.error(error);
+                      return -1;
+                    } else {
+                      this.catalogueDatabase.persistence.compactDatafile();
+                      response.json({
+                        status: "success",
+                      });
+                      return 0;
+                    }
+                  }
+                );
+              }
+            }
+          );
+        } catch (exception) {
+          console.error(
+            "Error in catalogueDirectory:catalogueDirectory.js:app.post(/removeCatalogue): ",
+            exception
+          );
+        }
       }
-    });
-  } catch (exception) {
-    console.error(
-      "Error in catalogueDirectory:catalogueDirectory.js:app.post(/removeCatalogue): ",
-      exception
     );
   }
-});
+
+  getApp() {
+    return this.app;
+  }
+};
+
+// class that holds a http(s) server application and its' configuration
+exports.Server = class Server {
+  key;
+  cert;
+  credentials;
+  httpServer;
+  httpsServer;
+
+  constructor(privateKeyFilename, certificateFilename, app) {
+    this.key = fs.readFileSync(privateKeyFilename);
+    this.cert = fs.readFileSync(certificateFilename);
+    this.credentials = {
+      key: this.key,
+      cert: this.cert,
+    };
+
+    this.run(app);
+  }
+
+  getHttpServer() {
+    return this.httpServer;
+  }
+
+  getHttpsServer() {
+    return this.httpsServer;
+  }
+
+  run(app) {
+    try {
+      // parse command line arguments
+      let commandLineArguments = process.argv.slice(2);
+
+      // check for proper usage
+      if (commandLineArguments.length != 2) {
+        console.error("Usage: node catalogueDirectory.js $PORT");
+        return;
+      } else {
+        this.httpServer = http.createServer(app);
+        this.httpsServer = https.createServer(this.credentials, app);
+        // run the server application
+        this.httpServer.listen(commandLineArguments[0], () =>
+          console.log(
+            `Catalogue Directory listening on http://localhost:${commandLineArguments[0]} ...`
+          )
+        );
+        this.httpsServer.listen(commandLineArguments[1], () =>
+          console.log(
+            `Catalogue Directory listening on https://localhost:${commandLineArguments[1]} ...`
+          )
+        );
+      }
+    } catch (exception) {
+      console.error(
+        "Error in catalogueDirectory:catalogueDirectory.js:setup(): ",
+        exception
+      );
+    }
+  }
+};
+
+let directory = new this.Directory("./catalogues.json");
+let app = new this.Application(directory.getDirectory());
+let server = new this.Server("private.key", "certificate.crt", app.getApp());
