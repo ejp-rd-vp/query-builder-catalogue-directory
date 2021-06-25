bbbb5836668ac4b6d96635fd68461538921f495a
David Reinert <david.reinert@kgu.de>
2021-06-25 09:43:46 +0200

    updated cors origin flag

--- directory/src/catalogueDirectory.ts
+++ directory/src/catalogueDirectory.ts
@@ -80,7 +80,7 @@ class Application {
       this.app = express();
       this.app.use(helmet());
       this.app.use(morgan("dev"));
-      this.app.use(cors());
+      this.app.use(cors({origin: 'http://localhost:3001'}));
       this.app.use("/ui", express.static("./public"));
       this.app.use(
         express.json({
